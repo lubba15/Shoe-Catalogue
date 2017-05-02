f651452d4b76c40ad0d62f827e62dc44f7006793
lubba15 <lubabalomkiva15@gmail.com>
2017-05-02 15:47:52 +0200

    Imade some changes

--- shoe.js
+++ shoe.js
@@ -1,89 +1,182 @@
-var shoes = [
-     {Brand : 'Nike', Color : 'Red', Size : 8, Price : 799, in_stock : 2},
-     {Brand : 'Nike', Color : 'Green', Size : 7, Price : 795, in_stock : 1},
-     {Brand : 'Puma', Color : 'Black', Size : 7, Price : 795, in_stock : 4},
-     {Brand : 'Puma', Color : 'Green', Size : 7, Price : 795, in_stock : 3},
-     {Brand : 'Adiddas', Color : 'Yellow', Size : 6, Price : 950, in_stock : 1},
-     {Brand : 'Adiddas', Color : 'Yellow', Size : 9,Price : 800, in_stock : 5}
+var shoes = [{
+    Brand: 'Nike',
+    Color: 'Red',
+    Size: 8,
+    Price: 799,
+    in_stock: 2
+  },
+  {
+    Brand: 'Nike',
+    Color: 'Green',
+    Size: 8,
+    Price: 795,
+    in_stock: 1
+  },
+  {
+    Brand: 'Puma',
+    Color: 'Green',
+    Size: 8,
+    Price: 795,
+    in_stock: 1
+  },
+  {
+    Brand: 'Adiddas',
+    Color: 'Green',
+    Size: 7,
+    Price: 795,
+    in_stock: 1
+  },
+  {
+    Brand: 'Puma',
+    Color: 'Black',
+    Size: 9,
+    Price: 795,
+    in_stock: 4
+  },
+  {
+    Brand: 'Puma',
+    Color: 'Green',
+    Size: 9,
+    Price: 795,
+    in_stock: 3
+  },
+  {
+    Brand: 'Adiddas',
+    Color: 'Yellow',
+    Size: 6,
+    Price: 950,
+    in_stock: 1
+  },
+  {
+    Brand: 'Adiddas',
+    Color: 'Black',
+    Size: 6,
+    Price: 800,
+    in_stock: 5
+  },
+  {
+    Brand: 'Adiddas',
+    Color: 'Black',
+    Size: 9,
+    Price: 800,
+    in_stock: 2
+  },
+  {
+    Brand: 'Adiddas',
+    Color: 'Yellow',
+    Size: 9,
+    Price: 800,
+    in_stock: 5
+  }
 ];
 
 var display = document.querySelector('.display');
 var output = document.querySelector('.output');
 
-var brandDrop = document.querySelector('.brands');
+var brand = document.querySelector('.brands');
 var size = document.querySelector('.sizes');
 var color = document.querySelector('.colors');
+var output = document.querySelector('.output');
 
 var dropDown = document.querySelector('.DropTamplate').innerHTML;
 var drop = Handlebars.compile(dropDown);
 
-var tableTamplate = document.querySelector('.tableTamplate').innerHTML;
-var table = Handlebars.compile(tableTamplate);
-
-function UniqueBrand(){
+function UniqueBrand() {
   var UniqueBrand = [];
   var brandMap = {};
-  for(var i = 0; i < shoes.length; i++){
+  for (var i = 0; i < shoes.length; i++) {
     var shoeBrand = shoes[i];
-    if (brandMap[shoeBrand.Brand] === undefined){
+    if (brandMap[shoeBrand.Brand] === undefined) {
       brandMap[shoeBrand.Brand] = shoeBrand.Brand;
       UniqueBrand.push(shoeBrand.Brand);
     }
   }
-  document. querySelector(".brands").innerHTML = drop({Brand:UniqueBrand});
+  var sorted = UniqueBrand.sort();
+  document.querySelector(".brands").innerHTML = drop({
+    dataList: UniqueBrand
+  });
 }
 UniqueBrand();
 
-function ColorUnique(){
+function ColorUnique() {
   var UniqueColor = [];
   var colorMap = {};
-   for(var i = 0; i < shoes.length; i++){
-     var shoeColor = shoes[i];
-     if (colorMap[shoeColor.Color] === undefined){
-       colorMap[shoeColor.Color] = shoeColor.Color;
-       UniqueColor.push(shoeColor.Color);
+  for (var i = 0; i < shoes.length; i++) {
+    var shoeColor = shoes[i];
+    if (colorMap[shoeColor.Color] === undefined) {
+      colorMap[shoeColor.Color] = shoeColor.Color;
+      UniqueColor.push(shoeColor.Color);
+    }
   }
-}
-document.querySelector(".colors").innerHTML = drop({Color:UniqueColor});
+  var sorted = UniqueColor.sort();
+  document.querySelector(".colors").innerHTML = drop({
+    dataList: UniqueColor
+  });
 }
 ColorUnique();
 
 function UniqueSize() {
   var UniqueSize = [];
   var sizeMap = {};
-  for(var i = 0; i < shoes.length; i++){
+  for (var i = 0; i < shoes.length; i++) {
     var shoeSize = shoes[i];
-    if(sizeMap[shoeSize.Size]=== undefined){
+    if (sizeMap[shoeSize.Size] === undefined) {
       sizeMap[shoeSize.Size] = shoeSize.Size;
       UniqueSize.push(shoeSize.Size);
     }
   }
-  document.querySelector(".sizes").innerHTML = drop({Size:UniqueSize});
+  var sorted = UniqueSize.sort();
+  document.querySelector(".sizes").innerHTML = drop({
+    dataList: UniqueSize
+  });
 }
 UniqueSize();
 
-function search() {
-  var searched = [];
-  for(var i = 0; i < shoes.length; i++){
-    var shoe = shoes[i];
-    console.log(brandDrop.value);
 
-    if(brandDrop.value === shoe.Brand && size.value == shoe.Size && color.value == shoe.Color){
-      searched.push(shoe);
+function search() {
+  var brand = document.querySelector('.brands');
+  var size = document.querySelector('.sizes');
+  var color = document.querySelector('.colors');
+
+  function brandfilter(input) {
+    return brand.value == input.Brand;
+  }
+
+  function colorfilter(input) {
+    return color.value == input.Color;
+  }
+
+  function sizefilter(input) {
+    //console.log(size.value);
+    return size.value == input.Size;
+  }
+  if (brand.value !== "") {
+    var shoeList = shoes.filter(brandfilter);
+  }
+  if (color.value !== "") {
+    if (brand.value !== "") {
+      var shoeList = shoeList.filter(colorfilter)
     }
-    else if(brandDrop.value === "All" && size.value == shoe.Size && color.value === "All"){
-      searched.push(shoe);
-    }
-    else if (brandDrop.value === shoe.brand && size.value == "All" && color.value ==="All" ){
-      searched.push(shoe);
+    var shoeList = shoes.filter(colorfilter);
+  }
+  if (size.value !== "") {
+    if (color.value !== "" && brand.value !== "") {
+      var shoeList = shoeList.filter(sizefilter);
+    } else {
+      var shoeList = shoes.filter(sizefilter);
+      // console.log(shoeList);
     }
   }
-  var restore = table({myShoe : searched})
+  var tamplate = document.querySelector('#tableTamplate').innerHTML;
+  var table = Handlebars.compile(tamplate);
 
+  var restore = table({
+    myShoe: shoeList
+  })
   output.innerHTML = restore;
 };
 
 
 function AddStock() {
-
-}
+ 
+};
